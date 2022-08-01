26751d57f997e2009e0850bb5c7b5e3ac6746ec5
burd5 <austinburdette5@gmail.com>
2022-07-31 19:12:24 -0600

    added another solution

--- detectPangram.js
+++ detectPangram.js
@@ -15,3 +15,12 @@ function isPangram(string){
       return false;
     }
   }
+
+  //every solution
+
+  function isPangram(string){
+    string = string.toLowerCase();
+    return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+      return string.indexOf(x) !== -1;
+    });
+  }
