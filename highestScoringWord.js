2c8cf039402844d36b4395e67354e067e607eebb
burd5 <austinburdette5@gmail.com>
2022-08-17 19:30:59 -0600

    added comment lines to last 6kyu challenge

--- highestScoringWord.js
+++ highestScoringWord.js
@@ -11,10 +11,15 @@ All letters will be lowercase and all inputs will be valid.
 */
 
 function high(x){
+    //creating variable to store alphabet letters
     let alphabet = '1abcdefghijklmnopqrstuvwxyz'
+    //splitting alphabet into an array to assign each letter to the value of it's index
     let arr = alphabet.split('');
+    //splitting the words in the original string
     let arrWords = x.split(' ');
+    //creating array to score the sum of the values of the letter indexes
     let arrScores = [];
+    //finding the index of each letter from the arr and pushing the sum to arrScores
     for(word of arrWords){
       let sum = 0
       for(let i = 0; i < word.length; i++){
@@ -22,12 +27,16 @@ function high(x){
       }
       arrScores.push(sum);
     }
+    //finding the max value from the arrScores
     let max = Math.max(...arrScores);
+    //creating another array to hold multiple words in the case they have the same max
     let highScores = [];
+    //pushing any word with the max score to highScores array
     for(let i = 0; i < arrScores.length; i++){
       if(arrScores[i] == max){
         highScores.push(arrWords[i])
       }
     }
+    //returning the first word that has the highest score
     return highScores[0];
   }
