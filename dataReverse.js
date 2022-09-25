a32d2d669962cb798db22e210319f8e16be9c8b5
burd5 <austinburdette5@gmail.com>
2022-09-25 10:36:27 -0600

    looked at solutions using for loops and splice

---
+++ dataReverse.js
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION:
+A stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+ */
+
+ const dataReverse = data => {
+    const bytes = [];
+    for (let i = 0; i < data.length; i += 8) {
+      bytes.unshift(...data.slice(i, i + 8));
+    }
+    return bytes;
+  };
+
+  //otherSolution
+  const dataReverse2 = data => {
+
+    const result = [];
+  
+    while(data.length){
+      result.push(...data.splice(-8))
+    }
+  
+    return result;
+  }
