e6ae33f1129ec4201d94699d7644aa57811ae662
burd5 <austinburdette5@gmail.com>
2022-10-26 10:41:39 -0600

    completed challenge using match and split/reverse/join

---
+++ palindromeForYourDome.js
@@ -0,0 +1,18 @@
+/*
+A palindrome is a word, phrase, number, or other sequence of symbols or elements, whose meaning may be interpreted the same way in either forward or reverse direction. Famous examples include "Amore, Roma", "A man, a plan, a canal: Panama" and "No 'x' in 'Nixon'". - wikipedia
+
+Our goal is to determine whether or not a given string is a valid palindrome or not.
+
+Like the above examples, here are a few test cases which are also populated:
+
+"Amore, Roma" => valid
+"A man, a plan, a canal: Panama" => valid
+"No 'x' in 'Nixon'" => valid
+"Abba Zabba, you're my only friend" => invalid
+*/
+
+function palindrome(string) {
+    let characters = /[a-z]/gi
+    let filterArr = string.toLowerCase().match(characters).join('');
+    return filterArr === filterArr.split('').reverse().join('');
+  }
